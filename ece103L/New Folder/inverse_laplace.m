62b21e5c27961148c6867267b39a2acbb25adf08
gabemg525 <102837294+gabemg525@users.noreply.github.com>
2023-08-09 09:46:57 -0700

    Delete ece103L directory

--- ece103L/New Folder/inverse_laplace.m
+++
@@ -1,5 +0,0 @@
-syms s t
-R = 1;
-f = 1 / (s^6+3.87*s^5+7.46*s^4+9.14*s^3+7.46*s^2+3.87*s+1);
-L = ilaplace(f,t);
-disp(['ilaplace(f) = ',char(L)]);
--- ece103L/lab3/lab3p1.m
+++
@@ -1,8 +0,0 @@
-%% diff eq of system: D2y+3Dy+2y= Dx, with BC y(0)=0 and vc(0)=5
-% impulse response: x=detla(t), B.C. hz(0)=0, Dhz(0)=1
-h_0=dsolve('2*D2hz+Dhz+4*hz=0','hz(0)=0','Dhz(0)=1','t');
-h=diff(h_0);
-disp(['impulse response h(t) = (',char(h),')u(t)']);
-to_plot=@(t) (exp(-t/4)*cos((31^(1/2)*t)/4) - (31^(1/2)*exp(-t/4)*sin((31^(1/2)*t)/4))/31).*(t>=0);
-fplot(to_plot,[-10 30]);
-xlabel ('t'); ylabel ('h(t)');
